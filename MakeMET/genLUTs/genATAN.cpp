a822b37d0294b967893a717e8caa5ef6c9fd38d6
Shivangi <shivangi.kamat@gmail.com>
2017-09-18 12:15:43 +0530

    update

--- MakeMET/genLUTs/genATAN.cpp
+++ MakeMET/genLUTs/genATAN.cpp
@@ -5,7 +5,7 @@
 #include <math.h>
 using namespace std;
 #define resolution 5
-//float pi = acos(-1.0);
+float pi = acos(-1.0);
 //printf("%d\n",pi);
 int max_val = 50;
 int size = (2*max_val)/resolution;
@@ -21,9 +21,9 @@ int main(int argc, char** argv) {
   for(int i = 0; i <= size; i ++) 
   {
     
-      value = atan(input); 
+    value = atan(input);  //value is in radians here
     input += resolution;
-    value
+    value = (value/pi)*180; //in degrees
       if(i!=size) 
     {
         cout << showbase << internal << setw(6) << setfill('0') << hex << value << ", ";
