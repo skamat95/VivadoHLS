6221c16a8f196f1c779d73eba0699dff9d737591
Shivangi <shivangi.kamat@gmail.com>
2017-09-19 15:58:11 +0530

    errors

--- Make_LUT_MET/test_Make_LUT_MET.c
+++ Make_LUT_MET/test_Make_LUT_MET.c
@@ -1,6 +1,6 @@
 #include <stdint.h>
 #include <stdio.h>
-
+#include <math.h>
 #include "Make_LUT_MET.h"
 
 
@@ -28,7 +28,7 @@ int main(int argc, char **argv) {
 		for(iCrd = 0; iCrd < NCrds; iCrd++) {
 			for(iRgn = 0; iRgn < NRgns; iRgn++) {
 				i = iCrt * NCrds * NRgns + iCrd * NRgns + iRgn;
-				rgnET[i] = 1;
+				rgnET[i] = 0.1;
 				rgnPhi[i] = 0;
 			}
 		}
@@ -45,8 +45,11 @@ int main(int argc, char **argv) {
 
 	printf("METx = %f\n",MET[0]);
 	printf("METy = %f\n",MET[1]);
-	printf("Theta_U = %f\n",MET[2]);
+	printf("Theta = %f\n",MET[2]);
 
+	//Calculating using math libraries
+	float real_t = atan2(MET[1],MET[0]);
+	printf("Test bench Theta = %f\n",real_t);
 	return 0;
 
 }
