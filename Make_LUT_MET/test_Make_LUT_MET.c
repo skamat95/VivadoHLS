234a6b3ee08ce348a1c82ad33e1eff893f5f31d0
Shivangi <shivangi.kamat@gmail.com>
2017-09-19 14:28:11 +0530

    new

--- Make_LUT_MET/test_Make_LUT_MET.c
+++ Make_LUT_MET/test_Make_LUT_MET.c
@@ -11,19 +11,12 @@
 
 int main(int argc, char **argv) {
 
-	int in = 90;
-	float out[3];
-	Make_LUT_MET(in,out);
-	printf("ans_sine = %f\n",out[0]);
-	printf("ans_cos = %f\n",out[1]);
-	printf("ans_atan = %f\n",out[2]);
-	/*
 	uint16_t rgnET[NCrts*NCrds*NRgns];
 	uint16_t rgnPhi[NCrts*NCrds*NRgns];
 	uint16_t hfET[NCrts*NHFRgns];
 	uint16_t hfPhi[NCrts*NHFRgns];
-	uint16_t MET[2];
-	uint64_t MET_theta[1];
+	float MET[2];
+	
 	// Test data; Construct it using indices for the fun of it
 
 	int iCrt;
@@ -36,27 +29,23 @@ int main(int argc, char **argv) {
 			for(iRgn = 0; iRgn < NRgns; iRgn++) {
 				i = iCrt * NCrds * NRgns + iCrd * NRgns + iRgn;
 				rgnET[i] = 1;
-				rgnPhi[i] = 1;
+				rgnPhi[i] = 0;
 			}
 		}
 		for(iHFRgn = 0; iHFRgn < NHFRgns; iHFRgn++) {
 			i = iCrt * NHFRgns + iHFRgn;
 			hfET[i] = 1;
-			hfPhi[i] = 1;
+			hfPhi[i] = 0;
 		}
 	}
 
 
 	//Test code
-	MakeMET(rgnET,rgnPhi,hfET,hfPhi,MET,MET_theta);
+	MakeMET(rgnET,rgnPhi,hfET,hfPhi,MET);
 
-	printf("METx = %d\n",MET[0]);
-	printf("METy = %d\n",MET[1]);
-	printf("Theta_U = %lld\n",MET_theta[0]);
-
-
-*/
-
+	printf("METx = %f\n",MET[0]);
+	printf("METy = %f\n",MET[1]);
+	printf("Theta_U = %f\n",MET[2]);
 
 	return 0;
 
