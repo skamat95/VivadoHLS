5cba60508d15f3551d37e16c81f3f9fc0b52e98a
Shivangi Kamat <skamat@uwlogin.cern.ch>
2018-04-26 13:43:24 +0200

    gct

--- GCT/GCT.cc
+++ GCT/GCT.cc
@@ -20,68 +20,20 @@ int GCT(uint16_t Tower[NCaloLayer2Eta][NCaloLayer2Phi_in],
 	uint16_t tower_num = 0;
 	uint16_t crystal_num = 0;
 
+	bool success[3][28] = false; //to keep a track of all clusters successful mergers or not
+
 	//For all clusters check if they are on the boundary
-	for(int i = 2; i < NCaloLayer2Phi_in ; i++){
+	for(int i = 2; i < NCaloLayer2Phi_in-2 ; i++){
 		for(int j = 0; j < NCaloLayer2ClustersPerPhi; j++){
 
 		if((Cluster_TowerID[j][i]%4) == 0){
 			//First boundary in tower
-			tower_num = Cluster_TowerID/4;
+			tower_num = Cluster_TowerID[j][i]/4;
+
 			if((Cluster_EtaPhi[j][1]%5) == 0){
 				//Then this crystal is on the boundary
 				crystal_num = Cluster_EtaPhi[j][i]/5;
-				//Find if there is a cluster in a tower next to it
-				if((i - 4) < 0){
-					//This is case 1, when cluster is on main boundary, so only two phi rows
-					for(int k = i - 2; k < i; k++){
-						for (int l = 0; l < NCaloLayer2ClustersPerPhi; l++){
-							if(((Cluster_TowerID[l][k]/4) == tower_num) && ((Cluster_TowerID[l][k]%4) == 3)){
-								//this means tower number is same
-								if(((Cluster_EtaPhi[l][k]/5) == crystal_num) && ((Cluster_EtaPhi[l][k]%5) == 4)){
-									//This is right next crystal
-								if(ClusterET[j][i] >= ClusterET[l][k]){
-									//add the ET
-									ClusterET[j][i] += ClusterET[l][k];
-									ClusterET[l][k] = 0;
-									//how to remove towerID and eta phi of other cluster
-								}
-								else{
-									ClusterET[l][k] +=  ClusterET[j][i];
-									ClusterET[j][i] = 0;
-									//remove towerID and eta phi
-								}
-
-								}
-							}
-						}
-					}
-				}
-				else{
-					//Case2: These have 4 phi rows to check as cluster is on internal boundary
-					for(int k = i - 4; k < i; k++){
-						for (int l = 0; l < NCaloLayer2ClustersPerPhi; l++){
-							if(((Cluster_TowerID[l][k]/4) == tower_num) && ((Cluster_TowerID[l][k]%4) == 3)){
-								//this means tower number is same
-								if(((Cluster_EtaPhi[l][k]/5) == crystal_num) && ((Cluster_EtaPhi[l][k]%5) == 4)){
-									//This is right next crystal
-								if(ClusterET[j][i] >= ClusterET[l][k]){
-									//add the ET
-									ClusterET[j][i] += ClusterET[l][k];
-									ClusterET[l][k] = 0;
-									//how to remove towerID and eta phi of other cluster
-								}
-								else{
-									ClusterET[l][k] +=  ClusterET[j][i];
-									ClusterET[j][i] = 0;
-									//remove towerID and eta phi
-								}
-
-								}
-							}
-						}
-					}
-				}
-
+				 success[j][i] = stitch_on_left(i,j,tower_num, crystal_num, ClusterET, Cluster_TowerID, Cluster_EtaPhi);
 
 
 			}
@@ -91,69 +43,15 @@ int GCT(uint16_t Tower[NCaloLayer2Eta][NCaloLayer2Phi_in],
 		if((Cluster_TowerID[j][i]%4) == 3){
 			//First boundary in tower
 			tower_num = Cluster_TowerID/4;
+
 			if((Cluster_EtaPhi[j][i]%5) == 4){
 				//Then this crystal is on second boundary
 				crystal_num = Cluster_EtaPhi[j][i]/5;
-				//Find if there is a cluster in a tower next to it
-				//Find if there is a cluster in a tower next to it
-				if((i + 4) > NCaloLayer2Phi_in){
-					//This is case 1, when cluster is on main boundary(on the other side), so only two phi rows
-					for(int k = i; k < i+2; k++){
-						for (int l = 0; l < NCaloLayer2ClustersPerPhi; l++){
-							if(((Cluster_TowerID[l][k]/4) == tower_num) && ((Cluster_TowerID[l][k]%4) == 0)){
-								//this means tower number is same
-								if(((Cluster_EtaPhi[l][k]/5) == crystal_num) && ((Cluster_EtaPhi[l][k]%5) == 0)){
-									//This is right next crystal
-								if(ClusterET[j][i] >= ClusterET[l][k]){
-									//add the ET
-									ClusterET[j][i] += ClusterET[l][k];
-									ClusterET[l][k] = 0;
-									//how to remove towerID and eta phi of other cluster
-								}
-								else{
-									ClusterET[l][k] +=  ClusterET[j][i];
-									ClusterET[j][i] = 0;
-									//remove towerID and eta phi
-								}
 
-								}
-							}
-						}
-					}
-				}
-				else{
-					//Case2: These have 4 phi rows to check as cluster is on internal boundary
-					for(int k = i; k < i + 4; k++){
-						for (int l = 0; l < NCaloLayer2ClustersPerPhi; l++){
-							if(((Cluster_TowerID[l][k]/4) == tower_num) && ((Cluster_TowerID[l][k]%4) == 0)){
-								//this means tower number is same
-								if(((Cluster_EtaPhi[l][k]/5) == crystal_num) && ((Cluster_EtaPhi[l][k]%5) == 0)){
-									//This is right next crystal
-								if(ClusterET[j][i] >= ClusterET[l][k]){
-									//add the ET
-									ClusterET[j][i] += ClusterET[l][k];
-									ClusterET[l][k] = 0;
-									//how to remove towerID and eta phi of other cluster
-								}
-								else{
-									ClusterET[l][k] +=  ClusterET[j][i];
-									ClusterET[j][i] = 0;
-									//remove towerID and eta phi
-								}
-
-								}
-							}
-						}
-
-
+				success[j][i] = stitch_on_right(i,j,tower_num, crystal_num, ClusterET, Cluster_TowerID, Cluster_EtaPhi);
 
 			}
 		}
-
-			}
-		}
-
-
 
 
 
@@ -163,4 +61,140 @@ int GCT(uint16_t Tower[NCaloLayer2Eta][NCaloLayer2Phi_in],
 
 
 return(0);
+}
+
+
+bool stitch_on_left(int i, int j, uint16_t tower_num, uint16_t crystal_num,
+		uint10_t ClusterET[NCaloLayer2ClustersPerPhi][NCaloLayer2Phi_in],
+		uint7_t Cluster_TowerID[NCaloLayer2ClustersPerPhi][NCaloLayer2Phi_in],
+	    uint5_t Cluster_EtaPhi[NCaloLayer2ClustersPerPhi][NCaloLayer2Phi_in]){
+
+
+	//Find if there is a cluster in a tower next to it
+	if((i - 4) < 0){
+		//Case 1: when cluster is on main boundary, so only two phi rows
+		for(int k = i - 2; k < i; k++){
+			for (int l = 0; l < NCaloLayer2ClustersPerPhi; l++){
+				if(((Cluster_TowerID[l][k]/4) == tower_num) && ((Cluster_TowerID[l][k]%4) == 3)){
+					//this means tower number is same
+					if(((Cluster_EtaPhi[l][k]/5) == crystal_num) && ((Cluster_EtaPhi[l][k]%5) == 4)){
+						//This is right next crystal
+					if(ClusterET[j][i] >= ClusterET[l][k]){
+						//add the ET
+						ClusterET[j][i] += ClusterET[l][k];
+						ClusterET[l][k] = 0;
+						//how to remove towerID and eta phi of other cluster
+						break;
+					}
+					else{
+						ClusterET[l][k] +=  ClusterET[j][i];
+						ClusterET[j][i] = 0;
+						//remove towerID and eta phi
+						break;
+					}
+
+					}
+				}
+			}
+		}
+	}
+	else{
+		//Case2: These have 4 phi rows to check as cluster is on internal boundary
+		for(int k = i - 4; k < i; k++){
+			for (int l = 0; l < NCaloLayer2ClustersPerPhi; l++){
+				if(((Cluster_TowerID[l][k]/4) == tower_num) && ((Cluster_TowerID[l][k]%4) == 3)){
+					//this means tower number is same
+					if(((Cluster_EtaPhi[l][k]/5) == crystal_num) && ((Cluster_EtaPhi[l][k]%5) == 4)){
+						//This is right next crystal
+					if(ClusterET[j][i] >= ClusterET[l][k]){
+						//add the ET
+						ClusterET[j][i] += ClusterET[l][k];
+						ClusterET[l][k] = 0;
+						//how to remove towerID and eta phi of other cluster
+						break;
+					}
+					else{
+						ClusterET[l][k] +=  ClusterET[j][i];
+						ClusterET[j][i] = 0;
+						//remove towerID and eta phi
+						break;
+					}
+
+					}
+				}
+			}
+		}
+	}
+
+return true;
+
+}
+
+
+
+bool stitch_on_right(int i, int j, uint16_t tower_num, uint16_t crystal_num,
+		uint10_t ClusterET[NCaloLayer2ClustersPerPhi][NCaloLayer2Phi_in],
+		uint7_t Cluster_TowerID[NCaloLayer2ClustersPerPhi][NCaloLayer2Phi_in],
+	    uint5_t Cluster_EtaPhi[NCaloLayer2ClustersPerPhi][NCaloLayer2Phi_in]){
+
+
+	//Find if there is a cluster in a tower next to it
+	if((i + 4) > NCaloLayer2Phi_in){
+		//This is case 1, when cluster is on main boundary(on the other side), so only two phi rows
+		for(int k = i; k < i+2; k++){
+			for (int l = 0; l < NCaloLayer2ClustersPerPhi; l++){
+				if(((Cluster_TowerID[l][k]/4) == tower_num) && ((Cluster_TowerID[l][k]%4) == 0)){
+					//this means tower number is same
+					if(((Cluster_EtaPhi[l][k]/5) == crystal_num) && ((Cluster_EtaPhi[l][k]%5) == 0)){
+						//This is right next crystal
+					if(ClusterET[j][i] >= ClusterET[l][k]){
+						//add the ET
+						ClusterET[j][i] += ClusterET[l][k];
+						ClusterET[l][k] = 0;
+						//how to remove towerID and eta phi of other cluster
+						break;
+					}
+					else{
+						ClusterET[l][k] +=  ClusterET[j][i];
+						ClusterET[j][i] = 0;
+						//remove towerID and eta phi
+						break;
+					}
+
+					}
+				}
+			}
+		}
+	}
+	else{
+		//Case2: These have 4 phi rows to check as cluster is on internal boundary
+		for(int k = i; k < i + 4; k++){
+			for (int l = 0; l < NCaloLayer2ClustersPerPhi; l++){
+				if(((Cluster_TowerID[l][k]/4) == tower_num) && ((Cluster_TowerID[l][k]%4) == 0)){
+					//this means tower number is same
+					if(((Cluster_EtaPhi[l][k]/5) == crystal_num) && ((Cluster_EtaPhi[l][k]%5) == 0)){
+						//This is right next crystal
+					if(ClusterET[j][i] >= ClusterET[l][k]){
+						//add the ET
+						ClusterET[j][i] += ClusterET[l][k];
+						ClusterET[l][k] = 0;
+						//how to remove towerID and eta phi of other cluster
+						break;
+					}
+					else{
+						ClusterET[l][k] +=  ClusterET[j][i];
+						ClusterET[j][i] = 0;
+						//remove towerID and eta phi
+						break;
+					}
+
+				}
+			}
+
+			}
+		}
+	}
+
+return true;
+
 }
--- GCT/GCT.hh
+++ GCT/GCT.hh
@@ -8,35 +8,39 @@
 #define uint5_t ap_uint<5>
 
 #include <stdint.h>
-const uint16_t NCaloLayer1Cards = 18;
-const uint16_t NCaloLayer1Eta = 17;
+const uint16_t NCaloLayer1Cards = 18;//1/2 eta
+const uint16_t NCaloLayer1Eta = 17;//1/2 eta
 const uint16_t NCaloLayer1Phi = 4;
 const uint16_t NCrystalsPerEtaPhi = 5;
 
 //According to one Layer 2 card
 const uint16_t NCaloLayer2Eta = 17; //17*2
-const uint16_t NCaloLayer2Phi_in = 28; //6*4
-const uint16_t NCaloLayer2Phi_out = 24;
+const uint16_t NCaloLayer2Phi_in = 28; //6*4 + 2 on either side
+const uint16_t NCaloLayer2Phi_out = 24; //6*4 as output per card
 
-const uint16_t NCaloEtaRegions = 2;
-const uint16_t NCaloPhiRegions = 6;
-const uint16_t NCaloLayer2Clusters = 12;
+const uint16_t NCaloEtaDirections = 2;
+const uint16_t NCaloPhiRegionsPerCard = 6;
+const uint16_t NCaloLayer2ClustersPerCard = 12;
 const uint16_t NCaloLayer2ClustersPerPhi = 3;
 
 const uint16_t NCaloLayer2ExtraPhi = 2;
 
-int GCT(uint16_t Tower[NCaloLayer2Eta][NCaloLayer2Phi],
-		uint10_t ClusterET[NCaloLayer2Clusters][NCaloPhiRegions][NCaloEtaRegions],
-		uint7_t Cluster_TowerID[NCaloLayer2Clusters][NCaloPhiRegions][NCaloEtaRegions],
-	    uint5_t Cluster_EtaPhi[NCaloLayer2Clusters][NCaloPhiRegions][NCaloEtaRegions],
-		uint16_t Tower_Left[NCaloLayer1Eta][NCaloLayer2ExtraPhi],
-		uint10_t ClusterET_Left[NCaloLayer2ClustersPerPhi],
-		uint7_t Cluster_TowerID_Left[NCaloLayer2ClustersPerPhi],
-		uint5_t Cluster_EtaPhi_Left[NCaloLayer2ClustersPerPhi],
-		uint16_t Tower_Right[NCaloLayer1Eta][NCaloLayer2ExtraPhi],
-		uint10_t ClusterET_Right[NCaloLayer2ClustersPerPhi],
-		uint7_t Cluster_TowerID_Right[NCaloLayer2ClustersPerPhi],
-		uint5_t Cluster_EtaPhi_Right[NCaloLayer2ClustersPerPhi]);
+int GCT(uint16_t Tower[NCaloLayer2Eta][NCaloLayer2Phi_in],
+		uint10_t ClusterET[NCaloLayer2ClustersPerPhi][NCaloLayer2Phi_in],
+		uint7_t Cluster_TowerID[NCaloLayer2ClustersPerPhi][NCaloLayer2Phi_in],
+	    uint5_t Cluster_EtaPhi[NCaloLayer2ClustersPerPhi][NCaloLayer2Phi_in]);
+
+bool stitch_on_left(int i, int j, uint16_t tower_num, uint16_t crystal_num,
+		uint10_t ClusterET[NCaloLayer2ClustersPerPhi][NCaloLayer2Phi_in],
+		uint7_t Cluster_TowerID[NCaloLayer2ClustersPerPhi][NCaloLayer2Phi_in],
+	    uint5_t Cluster_EtaPhi[NCaloLayer2ClustersPerPhi][NCaloLayer2Phi_in]);
+
+bool stitch_on_right(int i, int j, uint16_t tower_num, uint16_t crystal_num,
+		uint10_t ClusterET[NCaloLayer2ClustersPerPhi][NCaloLayer2Phi_in],
+		uint7_t Cluster_TowerID[NCaloLayer2ClustersPerPhi][NCaloLayer2Phi_in],
+	    uint5_t Cluster_EtaPhi[NCaloLayer2ClustersPerPhi][NCaloLayer2Phi_in]);
+
+
 
 
 #endif
